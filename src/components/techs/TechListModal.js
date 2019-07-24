d38215e24514c0acbb55c7f8fd6b5ca57f0aa914
Slava <ucheba00@gmail.com>
2019-07-24 14:18:08 -0600

    add tech and edit modal components

---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import TechItem from './TechItem';
+
+const TechsListModal = () => {
+  const [techs, setTechs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getTechs();
+    // eslint-disable-next-line
+  }, []);
+
+  const getTechs = async () => {
+    setLoading(true);
+    const res = await fetch('/techs');
+    const data = await res.json();
+
+    setTechs(data);
+    setLoading(false);
+  };
+
+  return (
+    <div id='tech-list-modal' className='modal'>
+      <div className='modal-content'>
+        <h4>Technitian List</h4>
+        <ul className='collection'>
+          {!loading &&
+            techs.map(tech => <TechItem tech={tech} key={tech.id} />)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TechsListModal;
