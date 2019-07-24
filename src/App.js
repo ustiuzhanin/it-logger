b5a54b986027eb9b93bf628833925c2a02a30cb4
Slava <ucheba00@gmail.com>
2019-07-24 15:38:20 -0600

    setup redux store and provider

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min';
@@ -17,17 +19,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <React.Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </React.Fragment>
+    <Provider store={store}>
+      <React.Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </React.Fragment>
+    </Provider>
   );
 };
 
