a8842ad18310848d7b187edf0a9243ec56288926
Slava <ucheba00@gmail.com>
2019-07-27 22:39:17 -0600

    add search functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   UPDATE_LOG,
   SET_CURRENT,
-  CLEAR_CURRENT
+  CLEAR_CURRENT,
+  SEARCH_LOGS
 } from './types';
 import { async } from 'q';
 
@@ -108,6 +109,26 @@ export const deleteLog = id => async dispatch => {
     dispatch({
       type: DELETE_LOG,
       payload: id
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+// search logs
+export const searchLogs = text => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
     });
   } catch (err) {
     dispatch({
