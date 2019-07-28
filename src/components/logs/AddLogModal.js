977e2d90bf287e870b2dd81f3bb165f9c4a7053c
Slava <ucheba00@gmail.com>
2019-07-27 21:49:49 -0600

    redux add and delete logs

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -10,7 +13,15 @@ const AddLogModal = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log(message, tech, attention);
+      const newLog = {
+        message,
+        tech,
+        attention,
+        data: new Date()
+      };
+
+      addLog(newLog);
+      M.toast({ html: `Log added by ${tech}` });
     }
 
     // clear fields
@@ -88,4 +99,11 @@ const modalStyle = {
   height: '75%'
 };
 
-export default AddLogModal;
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { addLog }
+)(AddLogModal);
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { deleteLog } from '../../actions/logActions';
 import Moment from 'react-moment';
 import PropTypes from 'prop-types';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min';
+
+const LogItem = ({ log, deleteLog }) => {
+  const onDelete = () => {
+    deleteLog(log.id);
+    M.toast({ html: 'Log Deleted' });
+  };
+
   return (
     <li className='collection-item'>
       <div>
@@ -21,7 +30,7 @@ const LogItem = ({ log }) => {
           <span className='black-text'> {log.tech}</span> on{' '}
           <Moment fortmat='MMMM Do YYYY, h:mm:ss a'>{log.data}</Moment>
         </span>
-        <a href='#!' className='secondary-content'>
+        <a href='#!' onClick={onDelete} className='secondary-content'>
           <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
@@ -30,7 +39,11 @@ const LogItem = ({ log }) => {
 };
 
 LogItem.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired
 };
 
-export default LogItem;
+export default connect(
+  null,
+  { deleteLog }
+)(LogItem);
